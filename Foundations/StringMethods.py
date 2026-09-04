0576e59be146c29b34b5d45c0f6e2b3880104bad
Aditya Srichandan <aditysrichandan275@gmail.com>
2026-09-04 20:42:45 +0530

    Add MatchCase.py and comment out string manipulation examples in StringMethods.py for clarity

--- Foundations/StringMethods.py
+++ Foundations/StringMethods.py
@@ -16,9 +16,20 @@
 
 # print(name.isupper())
 
-Name_With_Space = "    aditya srichandan    "
+# Name_With_Space = "    aditya srichandan    "
 
-CleanStructure = Name_With_Space.strip()
+# CleanStructure = Name_With_Space.strip()
 
-print(f"Original: {Name_With_Space}")
-print(f"Cleaned: {CleanStructure}")
+# print(f"Original: {Name_With_Space}")
+# print(f"Cleaned: {CleanStructure}")
+
+
+# name = "kanha srichandan"
+# print(name.split(" "))
+
+# print(name.split(" ")[1])
+
+# print(name.split(" ")[0].upper() + " " + name.split(" ")[1].capitalize())
+
+
+
