3df2731acc17a0c89f46d356142da4f0e7170c4f
Aditya Srichandan <aditysrichandan275@gmail.com>
2026-09-06 22:34:07 +0530

    Add examples for break and continue statements in BreakAndContinue.py

---
+++ Foundations/BreakAndContinue.py
@@ -0,0 +1,54 @@
+# for i in range( 1 , 12):
+#     if i == 7:
+#         break                         ## Exit from the loop
+#     print(i)
+
+
+# for i in range(1 , 12):
+#     if i == 10:
+#         print("skip here")
+#         continue                      ## skip when i == 10 then print 11
+#     print(i)
+
+
+# i = 0
+
+# while True:
+#     i += 1
+#     if i == 5:
+#         print("skip here")
+#         continue
+#     if i == 10:
+#         break
+#     print(i)
+
+
+# for i in range(1 , 12):
+#     if i == 7:
+#         print(f"skip here for {i}")
+#         continue  
+#     print(i)                    ## skip when i == 7 then print 8,9,10,11
+#     if i == 10:
+#         print(f"exit here for {i}")
+#         break                         ## Exit from the loop
+
+
+
+# for i in range(1 , 12):
+#     if i == 10:
+#         print(f"exit here for {i}")
+#         break                         # Exit immediately before printing the number
+        
+#     if i == 7:
+#         print(f"skip here for {i}")
+#         continue  
+        
+#     print(i)                    
+
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         if i == j:
+#             print(f"Skip For i={i}, j={j}")
+#             continue
+#         print(f"i={i}, j={j}")
