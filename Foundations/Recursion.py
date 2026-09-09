e22a0b44cb0404c3dab8f15349693fe50bf27bec
Aditya Srichandan <aditysrichandan275@gmail.com>
2026-09-09 22:21:50 +0530

    Add foundational examples for recursion with factorial and Fibonacci functions; include set operations in SetAndMethods.py

---
+++ Foundations/Recursion.py
@@ -0,0 +1,28 @@
+# def factorial(n):
+#     if(n == 0 or n == 1):
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
+
+# n = int(input("Enter a number to check: ".strip()))
+# print(f"Factorial of the number is {factorial(n)}")
+
+
+# def fibonacci(n):
+    
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+# n = int(input("Enter a number: "))
+# terms = n
+# print(f"fibonaaci sequence: ", end="")
+# for i in range(terms + 1):
+#     if i == terms:
+#         print(fibonacci(i))
+#     else:
+#         print(f"{fibonacci(i)} + ", end="")
---
+++ Foundations/SetAndMethods.py
@@ -0,0 +1,14 @@
+# set = {1, 2  ,3 , "Aditya"}
+# # print(type(set))
+
+# # set.add("python")
+# # print(set)
+
+# # copy_Set = set.copy()
+# # print(copy_Set)
+
+# new_set = [3 , 2 ,5 , 6 ,7 ,8,9 , "kanha"]
+
+# print(set.difference(new_set))     #reutn {1, 'Aditya'} 
+
+# print(set)
