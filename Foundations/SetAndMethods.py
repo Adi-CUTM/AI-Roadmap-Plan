be228466c4c07097ac74f92fa2b2dcf775d8d3d7
Aditya Srichandan <aditysrichandan275@gmail.com>
2026-09-10 22:45:11 +0530

    Uncomment set initialization and new_set definition in SetAndMethods.py; update set operations examples.

--- Foundations/SetAndMethods.py
+++ Foundations/SetAndMethods.py
@@ -1,4 +1,4 @@
-# set = {1, 2  ,3 , "Aditya"}
+set = {1, 2  ,3 , "Aditya"}
 # # print(type(set))
 
 # # set.add("python")
@@ -7,8 +7,10 @@
 # # copy_Set = set.copy()
 # # print(copy_Set)
 
-# new_set = [3 , 2 ,5 , 6 ,7 ,8,9 , "kanha"]
+new_set = [3 , 2 ,5 , 6 ,7 ,8,9 , "kanha"]
 
 # print(set.difference(new_set))     #reutn {1, 'Aditya'} 
+
+# set.difference_update(new_set)
 
 # print(set)
